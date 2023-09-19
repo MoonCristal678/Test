5dcbe18de2aa5f4fc9edf62a0ea10b8baf9baf08
Krystal <n01554329@humber.ca>
2023-09-19 10:17:58 -0400

    second push

--- fileOperations.js
+++ fileOperations.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
-
+const http = require('http');
+const path = require('path');
 // Function to write content to a file.
 function writeFile(filePath, content) {
   try {
@@ -10,6 +11,7 @@ function writeFile(filePath, content) {
     console.error('Error writing to the file:', err.message);
   }
 }
+
 
 // Function to read and display the content of a file.
 function readFile(filePath) {
@@ -22,6 +24,7 @@ function readFile(filePath) {
   }
 }
 
+
 // Function to delete a file.
 function deleteFile(filePath) {
   try {
@@ -32,12 +35,55 @@ function deleteFile(filePath) {
   }
 }
 
+
+
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    // Serve an HTML page with a button
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>JSON Data Page</title>
+      </head>
+      <body>
+        <h1>Welcome to the main page</h1>
+        <button onclick="redirectToJsonData()">Go to JSON Data</button>
+        <script>
+          function redirectToJsonData() {
+            window.location.href = '/api/users';
+          }
+        </script>
+      </body>
+      </html>
+    `;
+    res.end(htmlContent);
+  } else if (req.method === 'GET' && req.url === '/api/users') {
+    const filePath = path.join(__dirname, 'users.json');
+    readFile(filePath); // Read and display the content of the users.json file.
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const jsonData = [
+      { "id": 1, "name": "John" },
+      { "id": 2, "name": "Jane" }
+    ];
+    res.end(JSON.stringify(jsonData));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+
 // Function to take user input for file operations.
 function performFileOperations() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
+
 
   rl.question('Enter 1 to read a file, 2 to write content to a file, 3 to delete a file: ', (choice) => {
     if (choice === '1') {
@@ -63,7 +109,12 @@ function performFileOperations() {
     }
   });
 }
+server.listen(3000, () => {
+  console.log('Server running on <http://localhost:3000/>');
+});
+
 
 // Example usage:
 performFileOperations();
+
 
