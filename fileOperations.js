e9cb7f7cf5fb37b1156b70f98bf5b67602a70d6e
Krystal <n01554329@humber.ca>
2023-09-26 09:18:06 -0400

    push third

--- fileOperations.js
+++ fileOperations.js
@@ -1,7 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 const fs = require('fs');
-const readline = require('readline');
-const http = require('http');
 const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+
 // Function to write content to a file.
 function writeFile(filePath, content) {
   try {
@@ -9,9 +16,9 @@ function writeFile(filePath, content) {
     console.log(`File '${filePath}' successfully written.`);
   } catch (err) {
     console.error('Error writing to the file:', err.message);
+    throw err;
   }
 }
-
 
 // Function to read and display the content of a file.
 function readFile(filePath) {
@@ -19,11 +26,12 @@ function readFile(filePath) {
     const content = fs.readFileSync(filePath, 'utf8');
     console.log(`File content of '${filePath}':`);
     console.log(content);
+    return content;
   } catch (err) {
     console.error('Error reading the file:', err.message);
+    throw err;
   }
 }
-
 
 // Function to delete a file.
 function deleteFile(filePath) {
@@ -32,89 +40,113 @@ function deleteFile(filePath) {
     console.log(`File '${filePath}' successfully deleted.`);
   } catch (err) {
     console.error('Error deleting the file:', err.message);
+    throw err;
   }
 }
 
 
+// Serve an HTML page with buttons to perform file operations
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>File Operations Page</title>
+    </head>
+    <body>
+      <h1>Welcome to the File Operations Page</h1>
+      <button onclick="redirectToReadFile()">Read a File</button>
+      <button onclick="redirectToWriteFile()">Write to a File</button>
+      <button onclick="redirectToDeleteFile()">Delete a File</button>
+      <button onclick="redirectToApiUsers()">Display JSON Data</button>
+      <script>
+      function redirectToApiUsers() {
+         window.location.href = '/v1/api/users';
+      }
+</script>
+      <script>
+        function redirectToReadFile() {
+          window.location.href = '/v1/read';
+        }
+        function redirectToWriteFile() {
+          window.location.href = '/v1/write';
+        }
+        function redirectToDeleteFile() {
+          window.location.href = '/v1/delete';
+        }
+      </script>
+    </body>
+    </html>
+  `);
+});
 
+// API Version 1
+const v1Router = express.Router();
+v1Router.get('/api/users', (req, res) => {
+  const jsonData = [
+    { "id": 1, "name": "John" },
+    { "id": 2, "name": "Jane" }
+  ];
+  res.json(jsonData);
+});
 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    // Serve an HTML page with a button
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    const htmlContent = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>JSON Data Page</title>
-      </head>
-      <body>
-        <h1>Welcome to the main page</h1>
-        <button onclick="redirectToJsonData()">Go to JSON Data</button>
-        <script>
-          function redirectToJsonData() {
-            window.location.href = '/api/users';
-          }
-        </script>
-      </body>
-      </html>
-    `;
-    res.end(htmlContent);
-  } else if (req.method === 'GET' && req.url === '/api/users') {
-    const filePath = path.join(__dirname, 'users.json');
-    readFile(filePath); // Read and display the content of the users.json file.
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    const jsonData = [
-      { "id": 1, "name": "John" },
-      { "id": 2, "name": "Jane" }
-    ];
-    res.end(JSON.stringify(jsonData));
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+v1Router.get('/read', (req, res) => {
+  res.render('readFile.ejs');
+});
+
+v1Router.post('/read', (req, res) => {
+  const fileName = req.body.fileName;
+  const filePath = path.join(__dirname, fileName);
+
+  try {
+    const fileContent = readFile(filePath);
+    res.render('readFile.ejs', { fileContent });
+  } catch (err) {
+    res.render('readFile.ejs', { readError: 'Error reading the file.' });
   }
 });
 
-
-// Function to take user input for file operations.
-function performFileOperations() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-
-  rl.question('Enter 1 to read a file, 2 to write content to a file, 3 to delete a file: ', (choice) => {
-    if (choice === '1') {
-      rl.question('Enter the file name to read: ', (fileName) => {
-        rl.close();
-        readFile(fileName); // Read and display the content of the file.
-      });
-    } else if (choice === '2') {
-      rl.question('Enter the file name: ', (fileName) => {
-        rl.question('Enter content for the file: ', (content) => {
-          rl.close();
-          writeFile(fileName, content); // Create a new file with user-provided content and name.
-        });
-      });
-    } else if (choice === '3') {
-      rl.question('Enter the file name to delete: ', (fileName) => {
-        rl.close();
-        deleteFile(fileName); // Delete the specified file.
-      });
-    } else {
-      console.log('Invalid choice.');
-      rl.close();
-    }
-  });
-}
-server.listen(3000, () => {
-  console.log('Server running on <http://localhost:3000/>');
+v1Router.get('/write', (req, res) => {
+  res.render('writeFile.ejs');
 });
 
+v1Router.post('/write', (req, res) => {
+  const fileName = req.body.fileName;
+  const fileContent = req.body.fileContent;
 
-// Example usage:
-performFileOperations();
+  try {
+    writeFile(fileName, fileContent);
+    res.send(`File '${fileName}' created with the provided content.`);
+  } catch (err) {
+    res.status(500).send('Error creating the file.');
+  }
+});
 
+v1Router.get('/delete', (req, res) => {
+  res.render('deleteFile.ejs');
+});
 
+v1Router.post('/delete', (req, res) => {
+  const fileName = req.body.fileName;
+
+  try {
+    deleteFile(fileName);
+    res.send(`File '${fileName}' deleted.`);
+  } catch (err) {
+    res.status(500).send('Error deleting the file.');
+  }
+});
+
+// Use API versioning
+app.use('/v1', v1Router);
+
+// Define error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
